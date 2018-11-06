529e066a2e3345c53c6fc4311f9154c8ac844b88
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-11-06 12:23:00 +0100

    test

---
+++ TestDotNetCoreWebApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDotNetCoreWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CIBS World!");
+        }
+    }
+}
