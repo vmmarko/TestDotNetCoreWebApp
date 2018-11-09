c1a97dbc245e6edc754a65efff857aa4021246f2
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-11-09 04:59:59 +0100

    CritTest

---
+++ Test/CriticalTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class CriticalTests
+{
+    [Fact]
+    public void MyIntegrationTest()
+    {
+        // Do something
+        Console.WriteLine("Testing complete");
+    }
+}
