036526c9b88752d4a8a7d63655d01b4a3dd0d31b
vladislav.markovic <vladislav.markovic@wolterskluwer.com>
2018-11-11 09:51:57 +0100

    update

--- Test/CriticalTests.cs
+++ Test/CriticalTests.cs
@@ -8,6 +8,11 @@ public class CriticalTests
     public void MyIntegrationTest()
     {
         // Do something
-        Console.WriteLine("Testing complete");
+        Random irand = new Random.next(0, 50);
+        for (int i=0; i<rand; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Loop of " + rand + "\n" + "Testing complete !");
     }
 }
